ff5e304cc311212a6d2f39ed4af96c7dea5ef83a
shah-farheen <farheenshahdelhi@gmail.com>
2017-11-07 15:24:25 +0530

    created branch develop

--- app/src/main/java/com/bits/farheen/dhun/MainActivity.java
+++ app/src/main/java/com/bits/farheen/dhun/MainActivity.java
@@ -70,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        created branch develop
 
         ButterKnife.bind(this);
         mContext = this;
